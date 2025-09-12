0cb53462fb7c5749a05663dd1ecf8bb3e8a13e8d
Nemanja Stojanovic <13074352+nemanjas98@users.noreply.github.com>
2025-09-12 06:53:50 +0200

    Displays errors when user does not exist

--- src/test/java/com/nemanja/qa/tests/LoginTests.java
+++ src/test/java/com/nemanja/qa/tests/LoginTests.java
@@ -19,9 +19,27 @@ public class LoginTests extends BasicTest{
         navPage.getLoginButton().click();
         Assert.assertTrue(loginPage.getMailInput()
                            .getAttribute("type")
-                           .equals("email"), "Value for the type attribute should be email");
+                           .equals("email"),
+                           "Value for the type attribute should be email");
         Assert.assertTrue(loginPage.getPasswordInput()
                            .getAttribute("type")
-                           .equals("password"), "Value for the type attribute should be password");
+                           .equals("password"),
+                           "Value for the type attribute should be password");
+    }
+    
+    @Test (priority = 30)
+    public void DisplaysErrorsWhenUserDoesNotExist(){
+        navPage.getLoginButton().click();
+        loginPage.getMailInput().sendKeys("non-existing-user@gmail.com");
+        loginPage.getPasswordInput().sendKeys("password123");
+        loginPage.getLoginButton().click();
+        messagePopUpPage.waitForErrorPopUp();
+        Assert.assertTrue(messagePopUpPage.getTextMessageFromErrorPopUp()
+                           .getText()
+                           .contains("User does not exists"),
+                           "PopUp should contain 'User does not exists' text");
+        Assert.assertTrue(driver.getCurrentUrl()
+                           .contains("/login"),
+                           "Url should contain '/login'");
     }
 }
