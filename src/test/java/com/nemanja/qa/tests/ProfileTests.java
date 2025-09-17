90aa62d0e2df0f2366632207f95ebe603f7074f1
Nemanja Stojanovic <13074352+nemanjas98@users.noreply.github.com>
2025-09-17 19:49:54 +0200

    Edits profile

--- src/test/java/com/nemanja/qa/tests/ProfileTests.java
+++ src/test/java/com/nemanja/qa/tests/ProfileTests.java
@@ -13,6 +13,7 @@ public class ProfileTests extends BasicTest{
         loginPage.getPasswordInput().sendKeys("12345");
         loginPage.getLoginButton().click();
         navPage.getProfileButton().click();
+        
         Assert.assertTrue(driver.getCurrentUrl()
                           .contains("/profile"),
                           "Url should contain '/profile'");
@@ -21,6 +22,7 @@ public class ProfileTests extends BasicTest{
                           .getAttribute("value")
                           .equals("admin@admin.com"),
                           "Value should be 'admin@admin.com'");
+        
         navPage.getLogoutButton().click();
     }
     
@@ -37,7 +39,37 @@ public class ProfileTests extends BasicTest{
         Assert.assertFalse(profilePage.getMailInput()
                             .isEnabled(),
                             "Input should be disabled");  
+        
         navPage.getLogoutButton().click(); 
+    }
+    
+    @Test (priority = 30)
+    public void EditsProfile(){
+        navPage.getLoginButton().click();
+        loginPage.getMailInput().sendKeys("admin@admin.com");
+        loginPage.getPasswordInput().sendKeys("12345");
+        loginPage.getLoginButton().click();
+        navPage.getProfileButton().click();
+        profilePage.waitForPageToBeLoaded();
+        
+        profilePage.setName("Nemanja Stojanovic");
+        profilePage.setPhone("+381641234567");
+        profilePage.selectCity("Bucaramanga");
+        profilePage.setCountry("Spain");
+        profilePage.setTwitterUrl("https://twitter.com/profile/test123");
+        profilePage.setGitHubUrl("https://github.com/test123");
+        profilePage.getSaveButton().click();
+        
+        Assert.assertTrue(messagePopUpPage.waitForResponsePopUp()
+                          .isDisplayed(),
+                          "Pop up should be displayed");
+        Assert.assertTrue(messagePopUpPage.getTextMessageFromPopUp()
+                          .getText()
+                          .contains("Profile saved successfuly"),
+                          "PopUp should contain 'Profile saved successfuly' text");
+        profilePage.verifyInputValues();
+        
+        navPage.getLogoutButton().click();
     }
     
 }
